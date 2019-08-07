90952b9e38bb342c2ac9977edd3cb76dee3b6780
JessieW0010 <jessiewang@Jessies-MacBook-Pro.local>
2019-08-06 20:26:42 -0400

    appointments are now displayed

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,20 @@
 import React from "react";
 import "components/Appointment/styles.scss";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
+import Header from "components/Appointment/Header";
 
 export default function Appointment(props) {
+  let component;
+  if (props.interview) {
+    component = <Show student={props.interview.student} interviewer={props.interview.interviewer}/>
+  } else {
+    component = <Empty />
+  }
   return (
     <article className="appointment">
+      <Header time={props.time}/>
+      {component}
     </article>
   )
 }
